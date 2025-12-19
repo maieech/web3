43aee8834a6082be3bd6b3fa60bd2a159b4d3714
maieech <maleevmv@mer.ci.nsu.ru>
2025-12-19 17:13:50 +0700

    fix: :bug: Student entity group relation

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,9 +1,6 @@
-import { dbInit } from '@/db/AppDataSource';
 import { getGroupsDb } from '@/db/groupDb';
 
 export async function GET(): Promise<Response> {
-  await dbInit();
-
   const groups = await getGroupsDb();
 
   return new Response(JSON.stringify(groups), {
