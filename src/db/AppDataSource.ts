683344f68a9fc8ae0c4b4f5d39021a42cd882404
maieech <maleevmv@mer.ci.nsu.ru>
2025-12-19 17:13:48 +0700

    docs: :memo: Styles docs

--- src/db/AppDataSource.ts
+++ src/db/AppDataSource.ts
@@ -1,37 +1,30 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { Group } from './entity/Group.entity';
 import { Student } from './entity/Student.entity';
+import { Group } from './entity/Group.entity';
 
 const AppDataSource = new DataSource({
   type: 'sqlite',
   database: process.env.DB ?? './db/vki-web.db', // Path to your SQLite database file
-  entities: [Group, Student],
   synchronize: true, // Auto-create schema on startup (use with caution in production)
   logging: false,
+  entities: [Student, Group],
 });
 
 // to initialize the initial connection with the database, register all entities
 // and "synchronize" database schema, call "initialize()" method of a newly created database
 // once in your application bootstrap
-const init = async (): Promise<void> => {
+export const dbInit = async (): Promise<void> => {
+  console.log('>>> AppDataSource.initialize 0');
   try {
     await AppDataSource.initialize();
+    console.log('>>> AppDataSource.initialize');
   }
   catch (error) {
     console.log(error);
   }
 };
 
-await init();
-
-// AppDataSource.initialize()
-//   .then(() => {
-//     console.log('Data Source has been initialized!');
-//     // You can now interact with your entities
-//   })
-//   .catch((err) => {
-//     console.error('Error during Data Source initialization:', err);
-//   });
+await dbInit();
 
 export default AppDataSource;
