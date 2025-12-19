3b7ad9744c4eb574e4e1cf765ece93855b6d556e
maieech <maleevmv@mer.ci.nsu.ru>
2025-12-19 17:13:58 +0700

    feat: :wrench: Конфигурация для Postgres

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,6 +1,8 @@
+import { dbInit } from '@/db/AppDataSource';
 import { groupService } from '@/services/GroupService';
 
 export async function GET(): Promise<Response> {
+  await dbInit();
   const groups = await groupService.getGroups();
 
   return new Response(JSON.stringify(groups), {
--- src/app/api/students/add-test/route.ts
+++ src/app/api/students/add-test/route.ts
@@ -1,48 +1,9 @@
-import type GroupInterface from '@/types/GroupInterface';
-import { groupService } from '@/services/GroupService';
-
-const defaultGroups: GroupInterface[] = [
-  {
-    id: 1,
-    name: 'Group-1',
-    contacts: 'group1@test.test',
-  },
-  {
-    id: 2,
-    name: 'Group-2',
-    contacts: 'group2@test.test',
-  },
-  {
-    id: 3,
-    name: 'Group-3',
-    contacts: 'group3@test.test',
-  },
-  {
-    id: 4,
-    name: 'Group-4',
-    contacts: 'group4@test.test',
-  },
-];
+import { studentService } from '@/services/StudentService';
 
 export async function GET(): Promise<Response> {
-  const newGroups: GroupInterface[] = [];
-  const existGroups: GroupInterface[] = [];
+  const students = await studentService.addRandomStudents();
 
-  await Promise.all(defaultGroups.map(async (group) => {
-    const exists: GroupInterface = await groupService.getGroupsById(group.id);
-    if (!exists) {
-      const newGroup: GroupInterface = await groupService.addGroup(group);
-      newGroups.push(newGroup);
-    } else {
-      existGroups.push(exists);
-    }
-  }));
-
-  return new Response(JSON.stringify({
-    newGroups,
-    existGroups,
-  }), {
-    status: 201,
+  return new Response(JSON.stringify(students), {
     headers: {
       'Content-Type': 'application/json',
     },
